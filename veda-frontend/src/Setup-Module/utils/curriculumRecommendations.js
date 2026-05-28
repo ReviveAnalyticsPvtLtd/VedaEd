eda6c6b72bad720cf0f8b56e70bf6497414f2e04
Muskan Das <muskanndas@gmail.com>
2026-05-28 13:25:00 +0530

    fix: page 4

--- veda-frontend/src/Setup-Module/utils/curriculumRecommendations.js
+++ veda-frontend/src/Setup-Module/utils/curriculumRecommendations.js
@@ -1,6 +1,7 @@
 import {
   GRADE_OPTIONS,
   CURRICULUM_BOARDS_BY_COUNTRY,
+  INSTITUTION_TYPES,
 } from "../constants/schoolTypeCurriculum";
 import {
   generateRecommendation,
@@ -10,8 +11,23 @@ import { getGradeOrder } from "../recommendation/gradeUtils";
 
 export { getGradeOrder, generateRecommendation, getSmartCheckMessages };
 
-export function getGradeSelectOptions() {
-  return GRADE_OPTIONS.map((g) => ({ value: g.value, label: g.label }));
+const GRADE_OPTIONS_BY_INSTITUTION = {
+  [INSTITUTION_TYPES.PRESCHOOL]: ["Nursery", "LKG", "UKG"],
+  [INSTITUTION_TYPES.K12]: GRADE_OPTIONS.map((g) => g.value),
+  [INSTITUTION_TYPES.HIGHER_SECONDARY]: ["Grade 11", "Grade 12"],
+};
+
+export function getGradeSelectOptions(institutionType) {
+  const allowedGrades =
+    GRADE_OPTIONS_BY_INSTITUTION[institutionType] ||
+    GRADE_OPTIONS_BY_INSTITUTION[INSTITUTION_TYPES.K12];
+
+  return GRADE_OPTIONS.filter((g) => allowedGrades.includes(g.value)).map(
+    (g) => ({
+      value: g.value,
+      label: g.label,
+    })
+  );
 }
 
 export function getBoardOptionsForCountry(countryName) {
