1daadac5a86271d086c1f46eb354db6537fec478
Kartikey Somvanshi <kartikeysomvanshi79@gmail.com>
2026-08-03 23:06:49 +0530

    done

--- veda-backend/src/config/db.js
+++ veda-backend/src/config/db.js
@@ -1,8 +1,18 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 async function main() {
-    await mongoose.connect(process.env.db_Connect_String);
+    try {
+        console.log("Connecting...");
+        await mongoose.connect(process.env.db_Connect_String);
+        console.log("✅ MongoDB Connected");
+    } catch (err) {
+        console.error("❌ MongoDB Connection Failed:", err);
+        throw err;
+    }
 }
 
 module.exports = main;
